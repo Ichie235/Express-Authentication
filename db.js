2e9299c09ce55d0ec6d781dbf894ddb1cd7b8e7f
ichie chinemrem richard <chinemeremichie@gmail.com>
2022-12-30 21:59:29 +0100

    added views/ models/ and .env

---
+++ db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+function connectToDatabase(){
+    mongoose.connect(MONGODB_URI,{
+        dbName: "user-Auth",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+      },(err) =>
+      err ? console.log(err) : console.log(
+        "Connected to user-Auth database"))
+
+    mongoose.connection.on('connected',()=>{
+        console.log('connection to MongoDB was successful')
+    });
+
+    mongoose.connection.on('error',()=>{
+        console.log('connecting to MongoDB was unsuccessful')
+    })
+}
+
+module.exports = connectToDatabase
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const passport = require('passport')
+const connectEnsureLogin = require('connect-ensure-login')
+const connectToDatabase = require('./db')
 
+const PORT = process.env.PORT || 3000
 const app = express()
+
+//connect to MongoDB
+connectToDatabase()
 
 app.listen(PORT,()=>{
     console.log(`server is listening at http://localhost:${PORT}`)
